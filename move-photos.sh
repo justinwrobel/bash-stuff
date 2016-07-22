3e840f3ab9417eafb31cb06ddb9237377a70cd8d
jwrobel <jwrobel@wrob.co>
2016-07-21 23:22:25 -0400

    made industrial

--- move-photos.sh
+++ move-photos.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # This script will move jpgs in the current directory 
 # to year/month/day/$filename directory
 #TODO doc invokation
@@ -6,27 +7,54 @@
 #0x9003 - Date and Time
 #0x0132 - Date and Time (old)
 
-#TODO chang to copy rather than move
-#TODO copy to ~/tmp/all
 clean_filename(){
   f=${1//[^A-Za-z0-9\-\.]/_}
   f=${f,,}
   echo $f 
 }
 
-for i in *.jpg; do 
+dest="${!#}"
 
-manuf=$(exif -t 0x010f -m $i) 	#2
-model=$(exif -t 0x0110 -m $i) 	#
-dst1=$(exif -t 0x9003 -m $i) 	#2013:07:17 22:28:06
 
-#if 0x9003 didn't have anything try 0x0132
-if [ -z "$dst1" ]; then dst1=$(exif -t 0x0132 -m $i); fi 
-dst2=${dst1:0:7} 		#2013:07
-dst_dn=${dst2//://}
-dst_fn=${dst1//:/}
-dst_fn=${dst_fn// /_}
-clean=$(clean_filename "${dst_fn}_${manuf}_${model}.jpg")
-if [ -z "$dst2" ] && [ -z "$model" ] && [ -z "$manuf" ]; then echo "skipping $i"; continue; fi
-mkdir -p $dst_dn &&  mv "$i" "$dst_dn/$clean"
+for i ; do 
+
+  if [[ $i == $dest ]] ; then continue; fi
+  if [[ "${i,,}" != *"jpg" ]] ; then echo "$i isn't jpg. skipping"; continue; fi
+
+  #have defaults for everything aside from dst1
+  manuf=$(exif -t 0x010f -m "$i") 	#2
+  model=$(exif -t 0x0110 -m "$i") 	#
+  dst1=$(exif -t 0x9003 -m "$i") 	#2013:07:17 22:28:06
+
+  #if 0x9003 didn't have anything try 0x0132
+  if [ -z "$dst1" ]; then dst1=$(exif -t 0x0132 -m $i); fi 
+
+
+  #chop/clean the date up
+  dst2=${dst1:0:7} 		#2013:07
+  dst_dn=${dst2//://}
+  dst_fn=${dst1//:/}
+  dst_fn=${dst_fn// /_}
+  clean=$(clean_filename "${dst_fn}_${manuf}_${model}")
+
+  if [ -z "$model" ]; then model="unknown"; fi
+  
+  #skip if something is missing
+  if [ -z "$dst2" ] || [ -z "$model" ] || [ -z "$manuf" ]; then echo "skipping $i"; continue; fi
+
+  #check if file already exists and increment if needed
+  old_filename="$dest/$dst_dn/$clean.jpg" 
+  new_filename="$dest/$dst_dn/$clean.jpg" 
+  #inc=0
+  #while [ -f $new_filename ]; do 
+  #  inc=$((inc+1)) 
+  #  new_filename="$old_filename-$inc.jpg"
+  #done
+  #if [ $inc -gt 0 ]; then echo $new_filename was created due to conflict; fi
+
+  if [ -f $new_filename ] ; then
+    echo $new_filename already exists. Skipping.
+  else
+    mkdir -p "$dest/$dst_dn" && cp -i "$i" $new_filename 
+  fi
 done
