066d80a8d170e4af77996165fab0976db98c124f
Justin Wrobel <justin.wrobel@gmail.com>
2017-10-30 22:08:36 -0400

    Added dependency detection. Updated path detection to enable global invocation

--- photos/run-move-photos.sh
+++ photos/run-move-photos.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 here=$(pwd)
 
-#https://stackoverflow.com/a/4774063
-pushd `dirname $0` > /dev/null
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-popd > /dev/null
-
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTPATH
 
 remote="/run/user/1000/gvfs/smb-share:server=skylark,share="
 src="${remote}multimedia/Pictures/drop"
 dst="${remote}pictures/"
 
-
+# https://stackoverflow.com/a/677212
+command -v exif >/dev/null 2>&1 || { echo >&2 "I require exif but it's not installed.  Aborting."; exit 1; }
 [ ! -d "$src" ] && echo "$src not mounted" && exit 1
 [ ! -d "$dst" ] && echo "$dst not mounted" && exit 1
 
