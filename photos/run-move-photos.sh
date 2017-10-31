4b59f6503eed0a073582c82befa9e4cdebe81022
Justin Wrobel <justin.wrobel@gmail.com>
2017-10-30 21:14:25 -0400

    run-move-photos can be executed from anywhere

--- photos/run-move-photos.sh
+++ photos/run-move-photos.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+here=$(pwd)
+
+#https://stackoverflow.com/a/4774063
+pushd `dirname $0` > /dev/null
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+popd > /dev/null
+
+cd $SCRIPTPATH
+
 remote="/run/user/1000/gvfs/smb-share:server=skylark,share="
 src="${remote}multimedia/Pictures/drop"
 dst="${remote}pictures/"
 
+
 [ ! -d "$src" ] && echo "$src not mounted" && exit 1
 [ ! -d "$dst" ] && echo "$dst not mounted" && exit 1
 
-find ${remote}multimedia/Pictures/drop/ -iname "*.jpg" -exec ~/tmp/phones/move-photos.sh {} ${remote}pictures/ \;
+find ${remote}multimedia/Pictures/drop/ -iname "*.jpg" -exec ./move-photos.sh {} ${remote}pictures/ \;
+
+cd $here
