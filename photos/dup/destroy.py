4fbfa11075dbe21d3eed65c4af4e71b1bbbd05c5
Justin Wrobel <justin.wrobel@gmail.com>
2019-06-13 23:14:46 -0400

    enabled deletion and auto first pic

--- photos/dup/destroy.py
+++ photos/dup/destroy.py
@@ -67,57 +67,92 @@ class PictureRepo():
         #print ret
         return ret
 
+    def remove_filename(self, sig, filename):
+        print 'deleting image' + filename
+        os.remove(filename)
+
+        sig_file = self.DUP_DIR + sig
+        sig_file_tmp = self.DUP_DIR + sig + '.tmp'
+        print 'removing filename from ' + sig_file
+        with open(sig_file, 'r') as f1:
+            with open(self.DUP_DIR + sig + '.tmp', 'w') as f2:
+                for line in f1:
+                    if line.find(filename) == -1:
+                        f2.write(line)
+
+        os.rename(sig_file_tmp, sig_file)
+
+
 
 class MyImage(Image):
     pr = PictureRepo()
-    exif = DictProperty()
+    _exif = DictProperty()
+
+    @property
+    def exif(self):
+        print 'getting'
+        if not self._exif:
+            self._exif = self.pr.get_exif(self.source)
+        return self._exif
+
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
-            self.exif = self.pr.get_exif(self.source)
             return True
         return super(MyImage, self).on_touch_down(touch)
 
 
 class Controller(FloatLayout):
     pr = PictureRepo()
-    images_box = ObjectProperty(None)
-    images_compare_box = ObjectProperty(None)
+    images_box = GridLayout()
+    images_compare_box = GridLayout()
     info_box = ObjectProperty(None)
     delete_button = ObjectProperty(None)
     selected_image = DictProperty()
+    selected_sig = None
 
     def __init__(self, **kwargs):
         super(Controller, self).__init__(**kwargs)
-
-        for x in self.pr.get_signatures_with_dups():
+        sigs = self.pr.get_signatures_with_dups()
+        print len(sigs)
+        for x in sigs:
             btn = Button(id=x, text=x[0:10])
-            btn.bind(on_press=self.update_images)
+            btn.bind(on_press=self.select_sig)
             self.images_box.add_widget(btn)
 
         self.delete_button.bind(on_press=self.delete_selected)
 
-    def update_images(self, instance):
+    def select_sig(self, instance):
+        self.update_images(instance.id)
+        #print 'select first image' + self.images_compare_box.children[0]
+        # self.display_meta(self.images_compare_box.children[1], self.images_compare_box.children[1].exif)
+        self.select_image(self.images_compare_box.children[0], '')
+
+    def update_images(self, sig_id):
         self.images_compare_box.clear_widgets()
-        for x in self.pr.get_filename(instance.id):
+        self.selected_sig = sig_id
+        for x in self.pr.get_filename(sig_id):
             im = MyImage(source=x[7:])
             im.bind(on_touch_down=self.select_image)
-            im.bind(exif=self.display_meta)
+            #im.bind(exif=self.display_meta)
             self.images_compare_box.add_widget(im)
 
-    def delete_selected(self, instance):
-        print 'deleting image' + self.selected_image['JWFiledir']
-        print 'cleanup repo' + self.selected_image['JWFiledir']
-        print 'remove image from view'
 
-    def select_image(self,instance, event):
-        if( instance.exif):
+    def delete_selected(self, instance):
+        self.pr.remove_filename(self.selected_sig, self.selected_image['JWFilename'])
+        self.update_images(self.selected_sig)
+
+    def select_image(self, instance, event):
+        print(instance)
+
+        if(instance.exif):
             self.display_meta(instance, instance.exif)
 
     def display_meta(self, instance, exif):
+        print 'display_meta'
         self.selected_image = exif
         self.info_box.text = "\n".join([
-            exif['JWFiledir'][-40:], # TODO show last x chars
+            exif['JWFiledir'],
             exif['JWFilebase'],
             exif['DateTimeOriginal'],
             exif.get('Make', ''), exif.get('Model', ''),
