5a294fbf4c3e20c8083a804fbb9721809d18b2af
Justin Wrobel <justin.wrobel@gmail.com>
2020-05-07 23:58:28 -0400

    Updated move-photoes to have retry logic when moving files

--- photos/move-photos.sh
+++ photos/move-photos.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # This script will move jpgs in the current directory 
 # to year/month/day/$filename directory
-#TODO doc invocation
-# .../move-photos.sh */* . # move the files from sub dirs using . as a destination
+# ./move-photos.sh $source $destination
 #0x010f - Manufacturer 
 #0x0110 - Model
 #0x9003 - Date and Time
@@ -29,36 +28,46 @@ read_tag(){
   echo $r
 }
 
+move(){
+  src=$1
+  dst=$2
+  mkdir -p "$(dirname $dst)" \
+    && rsync -t "$src" "$dst" \
+    && rm "$src" \
+    && return 0 \
+    || echo "$? Issue processing $src" && return 1
+}
+
 if [[ $# < 1 ]] ; then echo "missing dest"; exit 1; fi
 
 dest="${!#}" #get last parameter
 
-for i ; do
+for src ; do
 
-  if [[ $i == $dest ]] ; then continue; fi
-  #${i,,} convert to lower case
-  if [[ "${i,,}" != *"jpg" ]] ; then echo "$i isn't jpg. skipping"; continue; fi
+  if [[ $src == $dest ]] ; then continue; fi
+  #${src,,} convert to lower case
+  if [[ "${src,,}" != *"jpg" ]] ; then echo "$src isn't jpg. skipping"; continue; fi
 
   #have defaults for everything aside from dst1
-  manuf=$(read_tag 0x010f "$i") 	#2
-  model=$(read_tag 0x0110 "$i") 	#
+  manuf=$(read_tag 0x010f "$src") 	#2
+  model=$(read_tag 0x0110 "$src") 	#
   if [ -z "$model" ] || [[ $model =~ "ExifData" ]]; then model="unknown"; fi
-  dst1=$(read_tag 0x9003 "$i") #2013:07:17 22:28:06, 2019-09-15T00:00:00Z
+  dst1=$(read_tag 0x9003 "$src") #2013:07:17 22:28:06, 2019-09-15T00:00:00Z
 
   datetime_pattern="([0-9]{4})[:-]([0-9]{2})[:-]([0-9]{2}).([0-9]{2})[:-]([0-9]{2})[:-]([0-9]{2})"
 
   #if dst1 isn't valid date time
   if [[ ! $dst1 =~ $datetime_pattern ]] ; then
-    dst2=$(read_tag 0x0132 "$i")
+    dst2=$(read_tag 0x0132 "$src")
     if [[ ! $dst2 =~ $datetime_pattern ]] || false ; then
-      echo "Invalid datetime detected ($dst1, $dst2). skipping $i."
+      echo "Invalid datetime detected ($dst1, $dst2). skipping $src."
       continue
     fi
   fi
 
-  if [ -z "$model" ] ; then echo "model is missing. skipping $i"; continue; fi
+  if [ -z "$model" ] ; then echo "model is missing. skipping $src"; continue; fi
   # BASH_REMATCH has the results of the last =~ comparison
-  if [[ ${#BASH_REMATCH[@]} < 7 ]]; then echo "datetime is missing. skipping $i"; continue; fi
+  if [[ ${#BASH_REMATCH[@]} < 7 ]]; then echo "datetime is missing. skipping $src"; continue; fi
   year=${BASH_REMATCH[1]}
   month=${BASH_REMATCH[2]}
   day=${BASH_REMATCH[3]}
@@ -73,7 +82,7 @@ for i ; do
   dst_fn="$year$month${day}_$hour$min$sec" #20130713_221330
 
   #remove img from filename
-  filename=$(remove_img "$i")
+  filename=$(remove_img "$src")
 
   #remove extra date pattern from filename
   [[ $filename =~ [-_]?[0-9]{8}[-_][0-9]{6}[-_]? ]] \
@@ -82,15 +91,15 @@ for i ; do
   clean=$(clean_filename "${dst_fn}_${model,,}_${filename,,}")
   clean=${clean/_\./\.} # remove trailing _
   old_clean=$(clean_filename "${dst_fn}_${manuf}_${model}.${extension}")
-  if [ "$i" == "$old_clean" ]; then 
+  if [ "$src" == "$old_clean" ]; then 
      clean=$(clean_filename "${dst_fn}_${model,,}.${extension}")
   fi
 
   #check if file already exists and increment if needed
-  old_filepath="$dest/$dst_dn/$clean" 
-  new_filepath="$dest/$dst_dn/$clean" 
+  old_filepath="$dest/$dst_dn/$clean"
+  new_filepath="$dest/$dst_dn/$clean"
   if [ -f "$new_filepath" ]; then
-     echo "Error while processing $i. $new_filepath already exists! Adding an increment."
+     echo "Error while processing $src. $new_filepath already exists! Adding an increment."
      inc=0
      #replace .jpg with $inc.jpg 
      while [ -f "$new_filepath" ]; do 
@@ -102,11 +111,7 @@ for i ; do
   if [ -f "$new_filepath" ] ; then
     echo $new_filepath already exists. Skipping.
   else
-#    echo  $new_filepath
-#    mkdir -p "$dest/$dst_dn" && cp -i "$i" "$new_filepath" #copy
-    mkdir -p "$dest/$dst_dn" \
-      && rsync -t "$i" "$new_filepath" \
-      && rm $i \
-      || echo "Issue processing $i"
+    # Retry from https://unix.stackexchange.com/a/82610/169986
+    for i in {1..5}; do move "$src" "$new_filepath" && break || sleep 1; done
   fi
 done
